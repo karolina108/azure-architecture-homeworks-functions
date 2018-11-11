7be92ce76ad5dcfaa65d8a5b2165eaf71dd143df
Karolina Boboli <project@karolinaboboli.com>
2018-11-11 13:29:02 +0100

    Added CLI and SQL scripts for creating environment and database table

---
+++ azure-scripts/sql_scripts.sql
@@ -0,0 +1,12 @@
+/* Create table */
+CREATE TABLE device_metrics (
+    id int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    device_id NVARCHAR(35),
+    metric_datetime DATETIME,
+    temperature NUMERIC(6, 2),
+    pressure NUMERIC (6, 2),
+    humidity NUMERIC (6, 2)
+);
+
+/* Check */
+SELECT * FROM device_metrics;
